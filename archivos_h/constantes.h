56db6edbdfa8ed8a7b356dc34c723c33449b412e
Franco Losardo <francolosa@hotmail.com>
2021-10-20 20:28:54 -0300

    Agrego archivo de constantes y algo de codigo

---
+++ archivos_h/constantes.h
@@ -0,0 +1,19 @@
+#ifndef CONSTANTES_H
+#define CONSTANTES_H
+#include <string>
+using namespace std;
+
+const char DELIMITADOR = ' ';
+
+const string RUTA_MATERIALES = "../archivos_de_texto/materiales.txt";
+const string RUTA_EDIFICIOS = "../archivos_de_texto/edificios.txt";
+const string RUTA_MAPA = "../archivos_de_texto/mapa.txt";
+const string RUTA_UBICACIONES = "../archivos_de_texto/ubicaciones.txt";
+const string LINEA_DIVISORIA = "\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550";
+
+const string COLOR_ROJO = "\e[38;5;196m";
+const string COLOR_POR_DEFECTO = "\e[0m";
+const string COLOR_VERDE = "\e[38;5;46m";
+const string COLOR_VERDE_AGUA = "\e[38;5;35m";
+const string COLOR_MARRON = "\e[38;5;136m";
+#endif
