e4844d3960b8b31f0e75db77531020acc5847cd6
franco <61808560+Franco-Losardo@users.noreply.github.com>
2021-10-21 20:37:23 -0300

    Borro las constantes y las paso a "constantes.h"

--- archivos_h/system.h
+++ archivos_h/system.h
@@ -15,11 +15,4 @@
 #define CLR_SCREEN "clear"
 #endif // __APPLE__
 
-#define PATH_MATERIALES "../archivos_de_texto/materiales.txt"
-#define PATH_EDIFICIOS "../archivos_de_texto/edificios.txt"
-#define PATH_UBICACIONES "../archivos_de_texto/ubicaciones.txt"
-#define PATH_MAPA "../archivos_de_texto/mapa.txt"
-
-const int EDIFICIOS_DISPONIBLES_MINIMO = 3
-
 #endif
